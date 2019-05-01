d88e845c96c1a9991a67c3d0d0530d17bde76396
Liz Lemon <nikorasu.dx@gmail.com>
2019-05-01 11:54:43 -0500

    troubleshooting heroku

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 const axios = require("axios");
-const secret = require("../keys");
 const bookQueries = require("../controller/bookController");
 
 //const gbook = secret.api.key;
